9728b5a79aa324a0835fb87eddcf565ce4bc0951
deadcodee1 <anaahmedasaad.4@gmail.com>
2024-11-20 17:10:43 +0200

    change icon app

---
+++ lib/features/Home/data/models/get_all_product_model.dart
@@ -0,0 +1,54 @@
+class ProductModel {
+  final String privetId;
+  final String title;
+  final String slug;
+  final String description;
+  final String imageCover;
+  final List<String> images;
+  final int price;
+  final int priceAfterDiscount;
+  final int quantity;
+  final String category;
+  final String occasion;
+  final String createAt;
+  final String updateAt;
+  final int v;
+  final String id;
+
+  ProductModel({
+    required this.privetId,
+    required this.title,
+    required this.slug,
+    required this.description,
+    required this.imageCover,
+    required this.images,
+    required this.price,
+    required this.priceAfterDiscount,
+    required this.quantity,
+    required this.category,
+    required this.occasion,
+    required this.createAt,
+    required this.updateAt,
+    required this.v,
+    required this.id,
+  });
+  factory ProductModel.fromJson(jsonData) {
+    return ProductModel(
+      privetId: jsonData["_id"],
+      title: jsonData["title"],
+      slug: jsonData["slug"],
+      description: jsonData["description"],
+      imageCover: jsonData["imgCover"],
+      images: List<String>.from(jsonData["images"]),
+      price: jsonData["price"],
+      priceAfterDiscount: jsonData["priceAfterDiscount"],
+      quantity: jsonData["quantity"],
+      category: jsonData["category"],
+      occasion: jsonData["occasion"],
+      createAt: jsonData["createdAt"],
+      updateAt: jsonData["updatedAt"],
+      v: jsonData["__v"],
+      id: jsonData["id"],
+    );
+  }
+}
