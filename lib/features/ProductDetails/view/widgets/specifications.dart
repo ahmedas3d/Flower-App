fc470633728126147693e0e4f67e7dc4c5254b47
deadcodee1 <anaahmedasaad.4@gmail.com>
2025-02-03 15:23:16 +0200

    add single child scroll view widget in Specifications , add expanded in product details and change color circular progress indicator

--- lib/features/ProductDetails/view/widgets/specifications.dart
+++ lib/features/ProductDetails/view/widgets/specifications.dart
@@ -12,99 +12,102 @@ class Specifications extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 12),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Text(
-                product.price.toString(),
-                style: TextStyle(
-                  fontSize: 20,
-                  color: AppColors.textColor1,
-                  fontWeight: FontWeight.w500,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(
+                  product.price.toString(),
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: AppColors.textColor1,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-              const Spacer(),
-              Text(
-                S.of(context).status,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: AppColors.textColor1,
-                  fontWeight: FontWeight.w500,
+                const Spacer(),
+                Text(
+                  S.of(context).status,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: AppColors.textColor1,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-              Text(
-                S.of(context).inStock,
-                style: TextStyle(
-                  fontSize: 14,
-                  color: AppColors.greyColor,
+                Text(
+                  S.of(context).inStock,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: AppColors.greyColor,
+                  ),
                 ),
+              ],
+            ),
+            Text(
+              S.of(context).allPricesIncludeTax,
+              style: TextStyle(
+                fontSize: 13,
+                color: AppColors.greyColor,
               ),
-            ],
-          ),
-          Text(
-            S.of(context).allPricesIncludeTax,
-            style: TextStyle(
-              fontSize: 13,
-              color: AppColors.greyColor,
             ),
-          ),
-          Text(
-            S.of(context).bouquet,
-            style: TextStyle(
-              fontSize: 16,
-              color: AppColors.textColor1,
-              fontWeight: FontWeight.w500,
+            Text(
+              S.of(context).bouquet,
+              style: TextStyle(
+                fontSize: 16,
+                color: AppColors.textColor1,
+                fontWeight: FontWeight.w500,
+              ),
             ),
-          ),
-          SizedBox(height: 15),
-          Text(
-            S.of(context).description.substring(2, 10),
-            style: TextStyle(
-              fontSize: 16,
-              color: AppColors.textColor1,
-              fontWeight: FontWeight.w500,
+            SizedBox(height: 15),
+            Text(
+              S.of(context).description.substring(2, 10),
+              style: TextStyle(
+                fontSize: 16,
+                color: AppColors.textColor1,
+                fontWeight: FontWeight.w500,
+              ),
             ),
-          ),
-          Text(
-            product.description,
-            style: TextStyle(
-              fontSize: 14,
-              color: AppColors.greyColor,
+            Text(
+              product.description,
+              style: TextStyle(
+                fontSize: 14,
+                color: AppColors.greyColor,
+              ),
             ),
-          ),
-          SizedBox(height: 15),
-          Text(
-            S.of(context).bouquetInclude,
-            style: TextStyle(
-              fontSize: 16,
-              color: AppColors.textColor1,
-              fontWeight: FontWeight.w500,
+            SizedBox(height: 15),
+            Text(
+              S.of(context).bouquetInclude,
+              style: TextStyle(
+                fontSize: 16,
+                color: AppColors.textColor1,
+                fontWeight: FontWeight.w500,
+              ),
             ),
-          ),
-          Text(
-            S.of(context).pinkRoses,
-            style: TextStyle(
-              fontSize: 14,
-              color: AppColors.textColor1,
+            Text(
+              S.of(context).pinkRoses,
+              style: TextStyle(
+                fontSize: 14,
+                color: AppColors.textColor1,
+              ),
             ),
-          ),
-          Text(
-            S.of(context).whiteWrap,
-            style: TextStyle(
-              fontSize: 14,
-              color: AppColors.textColor1,
+            Text(
+              S.of(context).whiteWrap,
+              style: TextStyle(
+                fontSize: 14,
+                color: AppColors.textColor1,
+              ),
             ),
-          ),
-          SizedBox(height: 25),
-          customButton(
-            title: S.of(context).addToCart,
-            onTap: () {},
-            color: AppColors.primaryColor,
-            textColor: AppColors.textColor2,
-          ),
-        ],
+            SizedBox(height: 25),
+            customButton(
+              title: S.of(context).addToCart,
+              onTap: () {},
+              color: AppColors.primaryColor,
+              textColor: AppColors.textColor2,
+            ),
+            SizedBox(height: 15),
+          ],
+        ),
       ),
     );
   }
